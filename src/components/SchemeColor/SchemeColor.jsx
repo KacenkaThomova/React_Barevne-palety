80077049f72645950c7c3e00affa872e2570792f
Katerina Thomova <113352240+KacenkaThomova@users.noreply.github.com>
2023-03-04 22:14:57 +0100

    Update SchemeColor.jsx

--- src/components/SchemeColor/SchemeColor.jsx
+++ src/components/SchemeColor/SchemeColor.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 
 export const SchemeColor = ({colorNumber}) => {
     return (
-    <div className="scheme-color" style={{ backgroundColor: {colorNumber} }} >{colorNumber}
+    <div className="scheme-color" style={{ backgroundColor: `${colorNumber}` }} >{colorNumber}
     </div> )
 }
